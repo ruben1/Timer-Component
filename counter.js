b507aff74bfdf5f7d45692c1034980499519052b
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-06-09 20:32:09 +0200

    add update timer

---
+++ counter.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  var counter = Object.create( HTMLElement.prototype );
+  counter.createdCallback = function() {
+    this.getAttributesData();
+    this.currentDate = this.startDate;
+    this.innerHTML = 
+      '<div class="timer_container">' +
+        '<div class="timer_seconds"></div>' +
+      '</div>';
+
+    this.secondsElement = this.querySelector('.timer_seconds');
+
+    this.updateTimerElement();
+
+    setInterval(function() {
+      this.updateTimerElement();
+    }.bind(this), 1000);
+  };
+
+  counter.getAttributesData = function() {
+    /**
+     * end-date(any format accepted by Date constructor): 
+     * start-date(any format accepted by Date constructor): 
+     * type: timer(default), stopwatch 
+     */
+    this.endDate = new Date(this.getAttribute('end-date') );
+    this.startDate = new Date( this.getAttribute('start-date') ) || new Date(); //get current time in seconds;
+    this.type = this.getAttribute('type') === undefined ? 'timer' : this.getAttribute('type');
+  };
+
+  counter.updateTimerElement = function() {
+    if(this.type === 'timer') {
+      this.currentDate.setSeconds(this.currentDate.getSeconds() + 1);
+      this.secondsElement.innerHTML = this.currentDate;
+    } else if(this.type === 'stopwatch') {
+      this.currentDate.setSeconds(this.currentDate.getSeconds() - 1);
+      this.secondsElement.innerHTML = this.currentDate;
+    }
+  };
+
+  counter.attributesChangedCallback = function() {
+
+  };
+
+  document.registerElement('counter-component', { 
+    prototype: counter 
+  });
+})();
