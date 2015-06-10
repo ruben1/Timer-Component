d70693946ccc3262c0ec47e8a9040abc889bea82
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2015-06-10 20:10:02 +0200

    add README

---
+++ README.md
@@ -0,0 +1,30 @@
+
+
+Compatibility
+=============
+
+Use a polyfill!!
+
+Check browsers compatibility
+
+How to use it
+=============
+
+1.- Check compatibility/add polyfill to your app
+2.- Insert counter.js into your app
+3.- Use the customer element 'counter-component'
+
+
+Description
+=======
+
+Attributes 
+--------------------------------
+type: 
+timer(default), stopwatch
+
+start-date: 
+any type of argument accepted by the Date constructor, default is now
+
+end-date: 
+any type of argument accepted by the Date constructor
--- script/counter.js
+++ script/counter.js
@@ -26,7 +26,6 @@
      * type: timer(default), stopwatch 
      */
     this.endDate = new Date(this.getAttribute('end-date') );
-    console.log('inital', this.endDate);
     this.startDate = !this.getAttribute('start-date') ? new Date() : new Date( this.getAttribute('start-date') ); //get current time in seconds if no input;
     this.type = !this.getAttribute('type') ? 'timer' : this.getAttribute('type'); //set type to timer if no input
   };
@@ -37,13 +36,17 @@
       this.secondsElement.innerHTML = currentDate;
     } else if(this.type === 'stopwatch') {    
       var timeLeft = ( this.endDate.getTime() - (new Date()).getTime() ) / 1000;
+      timeLeft = timeLeft < 0 ? 0 : timeLeft; 
       timeLeft = timeLeft.toString().indexOf('.') !== -1 ? timeLeft.toString().split('.')[0] : timeLeft;
       this.secondsElement.innerHTML = timeLeft;
     }
   };
 
-  counter.attributesChangedCallback = function() {
-
+  counter.attributeChangedCallback = function(attr, oldVal, newVal) {
+    if(/type|start-end|end-date/.test(attr)) {
+      this.getAttributesData();
+      this.updateTimerElement();
+    }
   };
 
   document.registerElement('counter-component', { 
